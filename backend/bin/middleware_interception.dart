c8827444266cb17d50deacd30f65e315ce000f83
WagaoCarvalhoDev <wagaocarvalhodev@gmail.com>
2023-03-11 20:29:20 -0300

    front consuming backend

---
+++ backend/bin/middleware_interception.dart
@@ -0,0 +1,37 @@
+import 'package:shelf/shelf.dart';
+
+class MInterception {
+  static Middleware get contentTypeJson => createMiddleware(
+        responseHandler: (Response res) => res.change(
+          headers: {
+            'content-type': 'application/json',
+            'xpto': '123',
+          },
+        ),
+      );
+
+  static Middleware get cors {
+    final headersPermitidos = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST, GET',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Max-Age': '86400',
+    };
+
+    Response? handlerOption(Request req) {
+      if (req.method == 'OPTIONS') {
+        return Response(200, headers: headersPermitidos);
+      } else {
+        return null;
+      }
+    }
+
+    Response addCorsHeader(Response res) =>
+        res.change(headers: headersPermitidos);
+
+    return createMiddleware(
+      requestHandler: handlerOption,
+      responseHandler: addCorsHeader,
+    );
+  }
+}
